2c7c3afb5bc5656eef7f5c2087aada5691d380d2
vinyborges <viny.bdj@gmail.com>
2023-02-01 22:38:10 -0800

    commit tree 3b02ba02649b034f15ffe2d33e9c4d58ce5d98eb

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpstreet){
+let distanceInBlocks;
+
+distanceInBlocks = pickUpstreet-42;
+return Math.abs(distanceInBlocks);
+
+}
+
+function distanceFromHqInFeet(someValue){
+let distanceInFeet = distanceFromHqInBlocks(someValue) * 264;
+return distanceInFeet;
+} 
+
+function distanceTravelledInFeet(start, destination){
+const distanceWalked = destination-start;
+const distanceTotal =  distanceWalked * 264;
+return Math.abs(distanceTotal);
+
+}
+
+function calculatesFarePrice (start,destination){
+let  distanceTravel;
+distanceTravel = distanceTravelledInFeet(start,destination);
+let travelFee;
+if(distanceTravel <=400){
+    travelFee = 0; 
+}
+    else if (distanceTravel>=400 & distanceTravel<=2000){
+        
+        travelFee = (distanceTravel-400)*0.02;
+    }
+        else if (distanceTravel >2000 & distanceTravel <=2500){
+            travelFee = 25;
+        }
+        else if (distanceTravel > 2500){
+            travelFee = 'cannot travel that far'
+        }
+
+return travelFee;
+}
